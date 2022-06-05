10928bf8c69429dc7bdc2c76aa42c61fbe2ea923
Paul Northrop <p.northrop@ucl.ac.uk>
2022-06-05 22:35:46 +0100

    Add tests

---
+++ tests/testthat/test-discontinuous.R
@@ -0,0 +1,27 @@
+# Check that for some continuous example functions that
+# (a) (b-a) / 2 is no larger than epsilon
+# (b) f(a) and f(b) are of opposite signs
+
+epsilon <- 1e-10
+
+# Staircase
+
+staircase <- function(x) ceiling(10 * x - 1) + 1 / 2
+res <- itp(staircase, c(-1, 1), epsilon = epsilon)
+test_that("Staircase: tolerance", {
+  testthat::expect_lte(res$b - res$a , 2 * epsilon)
+})
+test_that("Staircase: f(a) and f(b) of opposite signs", {
+  testthat::expect_false(sign(res$f.a) == sign(res$f.b))
+})
+
+# Trunc poly
+
+truncpoly <- function(x) (x / 2) ^ 2 + ceiling(x / 2) - 1 / 2
+res <- itp(truncpoly, c(-1, 1), epsilon = epsilon)
+test_that("Truncpoly: tolerance", {
+  testthat::expect_lte(res$b - res$a , 2 * epsilon)
+})
+test_that("Truncpoly: f(a) and f(b) of opposite signs", {
+  testthat::expect_false(sign(res$f.a) == sign(res$f.b))
+})
