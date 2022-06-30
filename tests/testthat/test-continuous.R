d101232361315b291b2eb3102e8531d34e3531c2
Paul Northrop <p.northrop@ucl.ac.uk>
2022-06-30 23:08:50 +0100

    Test unnamed function

--- tests/testthat/test-continuous.R
+++ tests/testthat/test-continuous.R
@@ -130,7 +130,7 @@ res <- itp(linear, c(-1, 0))
 test_that("Linear: solution at a", {
   testthat::expect_equal(c(res$iter, res$root, res$f.root), c(0, 0, 0))
 })
-res <- itp(linear, c(0, 1))
+res <- itp(function(x) x, c(0, 1))
 test_that("Linear: solution at b", {
   testthat::expect_equal(c(res$iter, res$root, res$f.root), c(0, 0, 0))
 })
